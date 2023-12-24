70e7d9b54606e111f50462a775d16f9b7dc8f35a
shamrei12 <61946511+shamrei12@users.noreply.github.com>
2023-12-25 02:14:59 +0300

    redesigned ProfileVC

--- PetFinder/View/Controllers/ProfileViewController.swift
+++ PetFinder/View/Controllers/ProfileViewController.swift
@@ -12,33 +12,22 @@ class ProfileViewController: UIViewController {
 
     private lazy var topView: UIView = {
         let topView = UIView()
-        topView.backgroundColor = UIColor(hex: 0xB7B7B7, alpha: 1)
-        topView.roundCorners(corners: [.layerMinXMaxYCorner, .layerMaxXMaxYCorner], radius: 12)
+        topView.backgroundColor = UIColor(hex: 0xEFBFA5, alpha: 1)
         return topView
     }()
     
-    private lazy var mainLabel: UILabel = {
-        let mainLabel = UILabel()
-        mainLabel.text = "Профиль"
-        mainLabel.font = UIFont.sfProText(ofSize: 24, weight: .semiBold)
-        mainLabel.textColor = .white
-        return mainLabel
-    }()
-    
-    private lazy var notificationButton: UIButton = {
-        let rightMainButton = UIButton()
-        rightMainButton.backgroundColor = .clear
-        rightMainButton.tintColor = .black
-        rightMainButton.layer.cornerRadius = 0
-        rightMainButton.setImage(UIImage(named: "bell"), for: .normal)
-        return rightMainButton
+    private lazy var userView: UIView = {
+        let userView = UIView()
+        userView.layer.cornerRadius = 16
+        userView.backgroundColor = UIColor(hex: 0xffffff, alpha: 1)
+        return userView
     }()
     
     private lazy var userImageView: UIImageView = {
         let userImageView = UIImageView()
         userImageView.clipsToBounds = true
 
-        let image = UIImage(named: "dog_back")
+        let image = UIImage(named: "placeholderPhoto")
         userImageView.image = image
         userImageView.contentMode = .scaleToFill
         userImageView.backgroundColor = .clear
@@ -48,74 +37,109 @@ class ProfileViewController: UIViewController {
     
     private lazy var userName: UILabel = {
         let userName = UILabel()
-        userName.text = "Дмитрий"
+        userName.text = "Ивановский Дмитрий"
+        userName.textAlignment = .center
         userName.font = UIFont.sfProText(ofSize: 16, weight: .regular)
         userName.textColor = UIColor(hex: 0x000000)
         return userName
     }()
     
-    private lazy var userDescription: UILabel = {
-        let userDescription = UILabel()
-        userDescription.text = "Какая-то инфа еще"
-        userDescription.font = UIFont.sfProText(ofSize: 16, weight: .regular)
-        userDescription.textColor = UIColor(hex: 0x000000)
-        return userDescription
+    private lazy var userAdsView: UIView = {
+        let userAdsView = UIView()
+        userAdsView.backgroundColor = UIColor(hex: 0xFCF4EF, alpha: 1)
+        userAdsView.layer.cornerRadius = 8
+        return userAdsView
     }()
     
-    private lazy var edit: UIButton = {
-        let edit = UIButton()
-        edit.setImage(UIImage(systemName: "pencil"), for: .normal)
-        edit.backgroundColor = UIColor(hex: 0xd9d9d9, alpha: 1)
-        return edit
+    private lazy var insertAdsView: UIView = {
+        let insertAdsView = UIView()
+        insertAdsView.backgroundColor = .white
+        return insertAdsView
     }()
     
-    private lazy var userMenu: UIStackView = {
-        let userMenu = UIStackView()
-        userMenu.axis = .vertical
-        userMenu.distribution = .fill
-        userMenu.alignment = .leading
-        userMenu.spacing = 26
-        return userMenu
+    private lazy var userAdsLabel: UILabel = {
+        let userAdsLabel = UILabel()
+        userAdsLabel.font = UIFont.sfProText(ofSize: 16, weight: .regular)
+        userAdsLabel.textColor = UIColor(hex: 0xFF975F, alpha: 1)
+        userAdsLabel.text = "Мои\nобъявления"
+        userAdsLabel.numberOfLines = 0
+        userAdsLabel.textAlignment = .center
+        return userAdsLabel
     }()
     
-    private lazy var userAds: UIButton = {
-        let userAds = UIButton()
-        userAds.setTitleColor(UIColor(hex: 0x000000), for: .normal)
-        userAds.setTitle("Мои объявления", for: .normal)
-        userAds.titleLabel?.font = UIFont.sfProText(ofSize: 20, weight: .regular)
-        return userAds
+    private lazy var userAdsCountLabel: UILabel = {
+        let userAdsCountLabel = UILabel()
+        userAdsCountLabel.font = UIFont.sfProText(ofSize: 20, weight: .semiBold)
+        userAdsCountLabel.textColor = UIColor(hex: 0xFF975F, alpha: 1)
+        userAdsCountLabel.text = "0"
+        userAdsCountLabel.textAlignment = .center
+        return userAdsCountLabel
     }()
     
-    private lazy var responses: UIButton = {
-        let responses = UIButton()
-        responses.setTitleColor(UIColor(hex: 0x000000), for: .normal)
-        responses.setTitle("Отклики", for: .normal)
-        responses.titleLabel?.font = UIFont.sfProText(ofSize: 20, weight: .regular)
-        return responses
+    private lazy var userRespondView: UIView = {
+        let userRespondView = UIView()
+        userRespondView.backgroundColor = UIColor(hex: 0xFCF4EF, alpha: 1)
+        userRespondView.layer.cornerRadius = 8
+        return userRespondView
     }()
     
-    private lazy var settings: UIButton = {
-        let settings = UIButton()
-        settings.setTitleColor(UIColor(hex: 0x000000), for: .normal)
-        settings.setTitle("Настройки профиля", for: .normal)
-        settings.titleLabel?.font = UIFont.sfProText(ofSize: 20, weight: .regular)
-        return settings
+    private lazy var insertRespondView: UIView = {
+        let insertRespondView = UIView()
+        insertRespondView.backgroundColor = .white
+        return insertRespondView
     }()
     
-    private lazy var сonfidetionality: UIButton = {
-        let сonfidetionality = UIButton()
-        сonfidetionality.setTitleColor(UIColor(hex: 0x000000), for: .normal)
-        сonfidetionality.setTitle("Настройки конфидециональности", for: .normal)
-        сonfidetionality.titleLabel?.font = UIFont.sfProText(ofSize: 20, weight: .regular)
-        return сonfidetionality
+    private lazy var userRespondLabel: UILabel = {
+        let userRespondLabel = UILabel()
+        userRespondLabel.font = UIFont.sfProText(ofSize: 16, weight: .regular)
+        userRespondLabel.textColor = UIColor(hex: 0xFF975F, alpha: 1)
+        userRespondLabel.text = "Мои\nотклики"
+        userRespondLabel.numberOfLines = 0
+        userRespondLabel.textAlignment = .center
+        return userRespondLabel
     }()
     
-    private lazy var exit: UIButton = {
-        let exit = UIButton()
-        exit.setTitleColor(UIColor(hex: 0x000000), for: .normal)
-        exit.setTitle("Выход", for: .normal)
-        exit.titleLabel?.font = UIFont.sfProText(ofSize: 20, weight: .regular)
-        return exit
+    private lazy var userRespondCountLabel: UILabel = {
+        let userRespondCountLabel = UILabel()
+        userRespondCountLabel.font = UIFont.sfProText(ofSize: 20, weight: .semiBold)
+        userRespondCountLabel.textColor = UIColor(hex: 0xFF975F, alpha: 1)
+        userRespondCountLabel.text = "0"
+        userRespondCountLabel.textAlignment = .center
+        return userRespondCountLabel
+    }()
+    
+    private lazy var settingsProfileView: UIView = {
+        let settingsProfileView = UIView()
+        settingsProfileView.backgroundColor = UIColor(hex: 0xffffff, alpha: 1)
+        settingsProfileView.layer.cornerRadius = 8
+        return settingsProfileView
+    }()
+    
+    private lazy var notificationButton: UIButton = {
+        let notification = UIButton()
+        notification.setTitle("Уведомления", for: .normal)
+        notification.setTitleColor(.label, for: .normal)
+        notification.setImage(UIImage(named: "bellProfile"), for: .normal)
+        notification.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -20)
+        return notification
+    }()
+    
+    private lazy var settingsButton: UIButton = {
+        let settingsButton = UIButton()
+        settingsButton.setTitle("Настройки", for: .normal)
+        settingsButton.setTitleColor(.label, for: .normal)
+        settingsButton.setImage(UIImage(named: "settingsProfile"), for: .normal)
+        settingsButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -20)
+        return settingsButton
+    }()
+    
+    private lazy var exitButton: UIButton = {
+        let exitButton = UIButton()
+        exitButton.setTitle("Выход", for: .normal)
+        exitButton.setTitleColor(.label, for: .normal)
+        exitButton.setImage(UIImage(named: "exitProfile"), for: .normal)
+        exitButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -20)
+        return exitButton
     }()
     
     override func viewDidLoad() {
@@ -125,29 +149,34 @@ class ProfileViewController: UIViewController {
     
     func setup() {
         setupUI()
-        setupUserMenu()
         makeConstraints()
     }
     
     func setupUI() {
         self.view.backgroundColor = UIColor(hex: 0xECECEC)
         self.view.addSubview(topView)
-        self.topView.addSubview(mainLabel)
-        self.topView.addSubview(userImageView)
-        self.topView.addSubview(notificationButton)
-        self.topView.addSubview(userName)
-        self.topView.addSubview(userDescription)
-        self.topView.addSubview(edit)
-        self.view.addSubview(userMenu)
+        self.view.addSubview(userView)
+        self.view.addSubview(userImageView)
+        self.userView.addSubview(userName)
+        
+        self.userView.addSubview(userAdsView)
+        self.userAdsView.addSubview(userAdsLabel)
+        self.userAdsView.addSubview(insertAdsView)
+        self.userAdsView.addSubview(userAdsCountLabel)
+        
+        self.userView.addSubview(userRespondView)
+        
+        self.userRespondView.addSubview(userRespondLabel)
+        self.userRespondView.addSubview(insertRespondView)
+        self.userRespondView.addSubview(userRespondCountLabel)
+        
+        self.view.addSubview(settingsProfileView)
+        self.settingsProfileView.addSubview(notificationButton)
+        self.settingsProfileView.addSubview(settingsButton)
+        self.settingsProfileView.addSubview(exitButton)
+ 
     }
-    
-    func setupUserMenu() {
-        userMenu.addArrangedSubview(userAds)
-        userMenu.addArrangedSubview(responses)
-        userMenu.addArrangedSubview(settings)
-        userMenu.addArrangedSubview(сonfidetionality)
-        userMenu.addArrangedSubview(exit)
-    }
+
     
     func makeConstraints() {
         topView.snp.makeConstraints { maker in
@@ -155,49 +184,95 @@ class ProfileViewController: UIViewController {
             maker.height.equalTo(240)
         }
         
-        mainLabel.snp.makeConstraints { maker in
-            maker.left.equalTo(self.view.safeAreaLayoutGuide).inset(20)
-            maker.top.equalTo(self.view.safeAreaLayoutGuide).inset(5)
-        }
-        
-        notificationButton.snp.makeConstraints { maker in
-            maker.right.equalTo(self.view.safeAreaLayoutGuide).inset(20)
-            maker.top.equalTo(self.view.safeAreaLayoutGuide).inset(5)
-            maker.width.equalTo(28)
-            maker.height.equalTo(notificationButton.snp.width)
+        userView.snp.makeConstraints { maker in
+            maker.top.equalToSuperview().inset(148)
+            maker.left.equalToSuperview().inset(16)
+            maker.right.equalToSuperview().inset(15)
+            maker.height.equalTo(252)
         }
         
         userImageView.snp.makeConstraints { maker in
-            maker.top.equalTo(mainLabel.snp.bottom).inset(-18)
-            maker.left.equalToSuperview().inset(16)
-            maker.height.width.equalTo(80)
+            maker.top.equalToSuperview().inset(73)
+            maker.centerX.equalToSuperview()
+            maker.height.width.width.equalTo(150)
         }
         
         userImageView.layoutIfNeeded()
         userImageView.layer.cornerRadius = userImageView.frame.width / 2
         
         userName.snp.makeConstraints { maker in
-            maker.top.equalTo(mainLabel.snp.bottom).inset(-21)
-            maker.left.equalTo(userImageView.snp.right).inset(-20)
+            maker.top.equalToSuperview().inset(82)
+            maker.left.right.equalToSuperview()
         }
         
-        userDescription.snp.makeConstraints { maker in
-            maker.top.equalTo(userName.snp.bottom).inset(-14)
-            maker.left.equalTo(userImageView.snp.right).inset(-20)
+        userAdsView.snp.makeConstraints { maker in
+            maker.top.equalTo(userName.snp.bottom).inset(-24)
+            maker.left.bottom.equalToSuperview().inset(24)
+            maker.width.equalTo(138)
+            maker.height.equalTo(100)
         }
         
-        edit.snp.makeConstraints { maker in
-            maker.top.equalTo(mainLabel.snp.bottom).inset(-18)
+        userAdsLabel.snp.makeConstraints { maker in
+            maker.top.equalToSuperview().inset(10)
+            maker.left.right.equalToSuperview().inset(24)
+        }
+        
+        insertAdsView.snp.makeConstraints {  maker in
+            maker.top.equalTo(userAdsLabel.snp.bottom).inset(-3)
+            maker.left.right.equalToSuperview().inset(11)
+            maker.height.equalTo(1)
+        }
+        
+        userAdsCountLabel.snp.makeConstraints { maker in
+            maker.top.equalTo(insertAdsView.snp.bottom).inset(-3)
+            maker.bottom.equalToSuperview().inset(10)
+            maker.centerX.equalToSuperview()
+        }
+        
+        userRespondView.snp.makeConstraints { maker in
+            maker.top.equalTo(userName.snp.bottom).inset(-24)
+            maker.right.equalToSuperview().inset(24)
+            maker.width.equalTo(138)
+            maker.height.equalTo(100)
+        }
+//        
+        userRespondLabel.snp.makeConstraints { maker in
+            maker.top.equalToSuperview().inset(10)
+            maker.left.right.equalToSuperview().inset(24)
+        }
+        
+        insertRespondView.snp.makeConstraints {  maker in
+            maker.top.equalTo(userRespondLabel.snp.bottom).inset(-3)
+            maker.left.right.equalToSuperview().inset(11)
+            maker.height.equalTo(1)
+        }
+        
+        userRespondCountLabel.snp.makeConstraints { maker in
+            maker.top.equalTo(insertRespondView.snp.bottom).inset(-3)
+            maker.bottom.equalToSuperview().inset(10)
+            maker.centerX.equalToSuperview()
+        }
+        
+        settingsProfileView.snp.makeConstraints { maker in
+            maker.top.equalTo(userView.snp.bottom).inset(-24)
+            maker.left.equalToSuperview().inset(16)
             maker.right.equalToSuperview().inset(15)
-            maker.width.height.equalTo(41)
+            maker.height.equalTo(156)
         }
         
-        edit.layoutIfNeeded()
-        edit.layer.cornerRadius = edit.frame.width / 2
+        notificationButton.snp.makeConstraints { maker in
+            maker.top.equalToSuperview().inset(20)
+            maker.left.equalToSuperview().inset(24)
+        }
         
-        userMenu.snp.makeConstraints { maker in
-            maker.top.equalTo(topView.snp.bottom).inset(-24)
-            maker.left.equalToSuperview().inset(18)
+        settingsButton.snp.makeConstraints { maker in
+            maker.top.equalTo(notificationButton.snp.bottom).inset(-16)
+            maker.left.equalToSuperview().inset(24)
+        }
+        
+        exitButton.snp.makeConstraints { maker in
+            maker.top.equalTo(settingsButton.snp.bottom).inset(-16)
+            maker.left.equalToSuperview().inset(24)
         }
         
     }
